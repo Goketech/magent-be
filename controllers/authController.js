3f3a07f57266f945dc9774ae74fa5cf3ad8b04af
Modupe Akanni <modupe775@gmail.com>
2025-04-20 23:27:11 +0100

    fix: auth

--- controllers/authController.js
+++ controllers/authController.js
@@ -102,14 +102,20 @@ const getNonce = async (req, res) => {
 };
 
 const verifySignature = async (req, res) => {
+  console.log("Verifying signature...");
   const { publicKey, signature } = req.body;
 
   if (!publicKey || !signature) {
     return res.status(400).json({ error: "Missing parameters" });
   }
 
+  console.log("Public Key:", publicKey);
+  console.log("Signature:", signature);
+
   const nonce = await NONCE_STORE.get(publicKey);
   if (!nonce) return res.status(400).json({ error: "Nonce not found" });
+
+  console.log("Nonce:", nonce);
 
   const verified = nacl.sign.detached.verify(
     Buffer.from(nonce),
@@ -129,6 +135,7 @@ const verifySignature = async (req, res) => {
 
     user = new User({
       walletAddress: publicKey,
+      email: publicKey,
       plan: freePlan._id,
       planEndDate: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000),
     });
