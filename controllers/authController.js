9743b1be4a601db1e378ce913fe6dc9b3d2305d4
Modupe Akanni <modupe775@gmail.com>
2025-06-20 11:49:57 +0100

    fix: response code

--- controllers/authController.js
+++ controllers/authController.js
@@ -71,7 +71,7 @@ const login = async (req, res) => {
     const user = await User.findOne({ email });
 
     if (!user || !(await user.comparePassword(password))) {
-      return res.status(401).json({ message: "Invalid credentials" });
+      return res.status(400).json({ message: "Invalid credentials" });
     }
 
     await User.updateOne(
