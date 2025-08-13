d54a578599f1127f5366846985783da0c2338eeb
prudentbird <prudentbird@gmail.com>
2025-08-14 00:35:00 +0100

    chore: update @effect/opentelemetry to version 0.56.2 and various dependency upgrades

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,33 +1,24 @@
+import { Injectable } from '@nestjs/common';
+import { Tracer } from '@effect/opentelemetry';
 import { Effect, Cause, Config } from 'effect';
-import { Injectable, HttpStatus } from '@nestjs/common';
-import { CustomHttpException } from './helpers/exception';
 
 @Injectable()
 export class AppService {
   getHello() {
     return Effect.gen(function* () {
+      const span = yield* Tracer.currentOtelSpan;
       return yield* Effect.succeed({
         message: 'Hello',
         data: {
-          version: yield* Config.string('npm_package_version'),
           uptime: process.uptime(),
+          traceId: span.spanContext().traceId ?? 'no-trace-id',
+          version: yield* Config.string('npm_package_version'),
           environment: yield* Config.string('NODE_ENV'),
         },
       });
     }).pipe(
+      Effect.withSpan('getHello'),
       Effect.tapError((err) => Effect.logError(err, Cause.fail(err))),
-      Effect.catchAll(() =>
-        Effect.fail(
-          new CustomHttpException(
-            {
-              message: 'Failed to get greeting',
-              traceId: 'no-trace-id',
-            },
-            HttpStatus.INTERNAL_SERVER_ERROR,
-          ),
-        ),
-      ),
-      Effect.withSpan('getHello', { attributes: { message: 'Hello' } }),
     );
   }
 }
